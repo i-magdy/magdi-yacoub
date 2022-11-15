8f33033aec152ef751777b04556542045809acea
Ibrahim Magdy <ibrahim.magdy31@gmail.com>
2022-11-15 20:33:49 +0200

    set dependency and some configs

--- app/src/main/java/org/myf/app/MYFApp.kt
+++ app/src/main/java/org/myf/app/MYFApp.kt
@@ -1,7 +1,12 @@
 package org.myf.app
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-
+@HiltAndroidApp
 class MYFApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
 }
--- app/src/main/java/org/myf/app/MainActivity.kt
+++ app/src/main/java/org/myf/app/MainActivity.kt
@@ -1,9 +1,9 @@
 package org.myf.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.myf.app.ui.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
