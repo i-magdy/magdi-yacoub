0807840003068decd6a1b199a95b0792ac595069
Ibrahim Magdy <ibrahim.magdy31@gmail.com>
2022-12-01 19:15:06 +0200

    Create upload report screen

---
+++ app/src/main/java/org/myf/ahc/ui/registration/reports/UploadReportsScreen.kt
@@ -0,0 +1,11 @@
+package org.myf.ahc.ui.registration.reports
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import org.myf.ahc.R
+
+@AndroidEntryPoint
+class UploadReportsScreen: Fragment(
+    R.layout.screen_upload_reports
+) {
+}
