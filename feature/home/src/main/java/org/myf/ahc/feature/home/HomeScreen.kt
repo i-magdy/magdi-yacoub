0c4684981c09825521874c5ddded30ebba81d7ec
Ibrahim Magdy <ibrahim.magdy31@gmail.com>
2023-01-21 14:02:05 +0200

    Update ReadMe

--- feature/home/src/main/java/org/myf/ahc/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/org/myf/ahc/feature/home/HomeScreen.kt
@@ -5,7 +5,6 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class HomeScreen : Fragment(
